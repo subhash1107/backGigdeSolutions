2eda3374615060fd2454293ca92542f81cea96aa
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-02 15:12:11 +0530

    modified login and signup to send user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,36 @@
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-const generateToken = (user) => jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-
+const generateToken = (user) =>
+  jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 export const register = async (req, res, next) => {
   try {
     const { name, email, password, country } = req.body;
     const userExists = await User.findOne({ email });
-    if (userExists) return res.status(400).json({ message: 'User already exists' });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
 
     const user = await User.create({ name, email, password, country });
-    res.status(201).json({ token: generateToken(user) });
+
+    res
+      .status(201)
+      .json({ token: generateToken(user), user: { name: user.name } });
   } catch (err) {
     next(err);
   }
 };
 
+// Login route
 export const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select("name email password");
+
     if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
-    res.json({ token: generateToken(user) });
+
+    res.json({ token: generateToken(user), user: { name: user.name } });
   } catch (err) {
     next(err);
   }
